07e605ffab687a05931aef7d089b784040a35597
Pablo Riera <pablo.riera@gmail.com>
2013-08-21 21:47:00 -0300

    WORKS

--- KinectCoreN/obj/x86_64Debug/addons/ofxXmlSettings/libs/tinyxmlerror.d
+++
@@ -1,5 +0,0 @@
-obj/x86_64Debug/addons/ofxXmlSettings/libs/tinyxmlerror.o: \
- /usr/local/of_v0.7.4_linux64_release/addons/ofxXmlSettings/libs/tinyxmlerror.cpp \
- /usr/local/of_v0.7.4_linux64_release/addons/ofxXmlSettings/libs/tinyxml.h
-
-/usr/local/of_v0.7.4_linux64_release/addons/ofxXmlSettings/libs/tinyxml.h:
