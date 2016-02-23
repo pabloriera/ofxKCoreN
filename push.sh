4422a431a628f1c3c2b04d334453024fd78c5d08
rieritico <pablo.riera@gmail.com>
2016-02-23 02:29:40 -0300

    order

--- push.sh
+++ push.sh
@@ -1,6 +1,8 @@
 echo "-------------------------------------->"
 echo "CLEANING..."
+cd KinectCoreN
 make clean
+cd ..
 echo "-------------------------------------->"
 echo "COMMITING..."
 git commit -a -m $1
